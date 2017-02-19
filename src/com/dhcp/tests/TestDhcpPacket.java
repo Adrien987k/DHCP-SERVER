b388019a9394e2e84b0cd80c6069667780989fb0
ArnCLAUDEL <arnaud-claudel@hotmail.fr>
2017-02-19 21:31:50 +0100

    test d'envoi de paquet UDP

--- src/com/dhcp/tests/TestDhcpPacket.java
+++ src/com/dhcp/tests/TestDhcpPacket.java
@@ -3,7 +3,9 @@ package com.dhcp.tests;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 
 import com.dhcp.message.DhcpMessage;
 
@@ -11,7 +13,56 @@ public class TestDhcpPacket {
 	public static void main(String[] args) {
 		boolean stop = false;
 		int port = 67;
+		byte[] messageTest = new byte[] {1,1,0,0,1,1};
 		
+		
+		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					DatagramSocket ds1234 = new DatagramSocket(1234);
+					try {
+						byte[] handler;
+						ds1234.receive(new DatagramPacket((handler = new byte[messageTest.length]), messageTest.length));
+						System.out.println("Received: " + handler.toString());
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				} catch (SocketException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		
+		DatagramSocket ds;
+		for(int i = 0; i < 5; i++) {
+		try {
+			ds = new DatagramSocket();
+			ds.setBroadcast(true);
+			
+			
+			DatagramPacket message=new DatagramPacket(messageTest
+					,messageTest.length
+					,InetAddress.getByName("google.com")
+					,1234);
+			System.out.println("message created " + message.toString());
+			ds.send(message);
+		} catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnknownHostException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		}	
+		/*
 		try(DatagramSocket ds = new DatagramSocket(port)) {
 			ds.setSoTimeout(1000);
 			while(!stop) {
@@ -31,6 +82,6 @@ public class TestDhcpPacket {
 			}
 		} catch (SocketException se) {
 			se.printStackTrace();
-		}
+		}*/
 	}
 }
