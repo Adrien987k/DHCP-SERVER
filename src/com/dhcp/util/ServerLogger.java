f8f4bdc22e8240e782c393d5fb7e2b63b453940d
ArnCLAUDEL <arnaud-claudel@hotmail.fr>
2017-03-01 22:33:16 +0100

    ajout methode message envoyé + correctif d'affichage

--- src/com/dhcp/util/ServerLogger.java
+++ src/com/dhcp/util/ServerLogger.java
@@ -3,10 +3,14 @@ package com.dhcp.util;
 public class ServerLogger {
 	
 	public void systemMessage(String s) {
-		System.out.println(" -- SYSTEM MESSAGE -- " + s + "\n");
+		System.out.println(" -- SYSTEM MESSAGE -- \n" + s + "\n");
 	}
 	
 	public void messageReceived(String s){
 		System.out.println(" -- MESSAGE RECEIVED -- \n" + s + "\n");
+	}
+	
+	public void messageSent(String s){
+		System.out.println(" -- MESSAGE SENT -- \n" + s + "\n");
 	}
 }
